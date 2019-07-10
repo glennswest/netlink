48e59255c6083ba08b2b8343192c51fa3ed64a39
gwest <gwest@redhat.com>
2019-07-10 08:27:59 +0800

    Move constant definition

--- netlink.go
+++ netlink.go
@@ -11,6 +11,16 @@ package netlink
 import (
 	"errors"
 	"net"
+        "github.com/vishvananda/netlink/nl"
+)
+
+
+// Family type definitions
+const (
+        FAMILY_ALL  = nl.FAMILY_ALL
+        FAMILY_V4   = nl.FAMILY_V4
+        FAMILY_V6   = nl.FAMILY_V6
+        FAMILY_MPLS = nl.FAMILY_MPLS
 )
 
 var (
